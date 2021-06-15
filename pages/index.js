f938ea3bb5d316be3d43d6e2e55fee1713c3c11c
nicholas-bd <nicholas.bond.d@gmail.com>
2021-06-15 20:51:46 +0200

    Added new files and removed unused

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,30 @@ export default function Home() {
           </a>
         </div>
       </div>
+      <div className="card">
+        <div className="card_content">
+          <h2 className="card_title">Something marvelous</h2>
+          <p className="card_body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio,
+            minus! Vitae alias
+          </p>
+          <a href="#" className="button">
+            See more
+          </a>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card_content">
+          <h2 className="card_title">Something marvelous</h2>
+          <p className="card_body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio,
+            minus! Vitae alias
+          </p>
+          <a href="#" className="button">
+            See more
+          </a>
+        </div>
+      </div>
     </div>
   );
 }

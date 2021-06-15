dbc576015d326505b078f778ef2c95a47f8de234
nicholas-bd <nicholas.bond.d@gmail.com>
2021-06-15 20:21:14 +0200

    Added styles and animations

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,16 @@ export default function Home() {
   return (
     <div className="wrapper">
       <div className="card">
-        <h2 className="card_title">Something marvelous</h2>
-        <p className="card_body">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, minus!
-          Vitae alias
-        </p>
-        <a href="#" className="button"></a>
+        <div className="card_content">
+          <h2 className="card_title">Something marvelous</h2>
+          <p className="card_body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio,
+            minus! Vitae alias
+          </p>
+          <a href="#" className="button">
+            See more
+          </a>
+        </div>
       </div>
     </div>
   );
